6e3c9ea9c6d137f493d959fba40345c19070f2cc
hwrk <psibladestrinke@gmail.com>
2025-11-21 21:59:24 +0400

    Change origin

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const secret = process.env.JWT_SECRET
 
 //Middlewares 
 app.use(cors({
-        origin:"http://localhost:5173",
+        origin: "https://hwrk.vercel.app", //"http://localhost:5173",
         methods:["GET","POST","DELETE","PUT"]
 }
 
